98fe2e61e0579a679c36cda37605a2d73b8667c3
r00t <algocerebro@gmail.com>
2020-04-14 11:30:06 +0600

    Replace reach router

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -8,8 +8,7 @@ import React, { Component} from 'react';
 import '../styles/Navbar.css';
 import Report from './Report'
 import Prescriptions from './Prescriptions'
-import { Link } from "@reach/router"
-
+import { Link } from 'react-router-dom'
 
 
   class Navbar extends Component {
@@ -17,15 +16,15 @@ import { Link } from "@reach/router"
           return (
             <Breadcrumb className="navbar" bg="#FEFCBF" width="100%" height="100px" spacing="20px">
                 <BreadcrumbItem>
-                    <BreadcrumbLink className="link" color="#5F370E" as={Link} to={process.env.PUBLIC_URL + '/'}>সেবা সম্পর্কে</BreadcrumbLink>
+                    <BreadcrumbLink className="link" color="#5F370E" as={Link} to='/'>সেবা সম্পর্কে</BreadcrumbLink>
                 </BreadcrumbItem>
 
                 <BreadcrumbItem>
-                    <BreadcrumbLink className="link" color="#5F370E" as={Link} to={process.env.PUBLIC_URL + '/prescriptions'}>প্রেসক্রিপশন সমূহ</BreadcrumbLink>
+                    <BreadcrumbLink className="link" color="#5F370E" as={Link} to='/prescriptions'>প্রেসক্রিপশন সমূহ</BreadcrumbLink>
                 </BreadcrumbItem>
 
                 <BreadcrumbItem>
-                    <BreadcrumbLink className="link" color="#5F370E" as={Link} to={process.env.PUBLIC_URL + '/report'}>রিপোর্ট তৈরি করুন</BreadcrumbLink>
+                    <BreadcrumbLink className="link" color="#5F370E" as={Link} to='/report'>রিপোর্ট তৈরি করুন</BreadcrumbLink>
                 </BreadcrumbItem>
 
                 <BreadcrumbItem>
