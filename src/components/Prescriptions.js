2e8910fb0f5ec114261a1f4fc3f77a4dc42892df
r00t <algocerebro@gmail.com>
2020-04-14 14:50:12 +0600

    Patient side mockup

--- src/components/Prescriptions.js
+++ src/components/Prescriptions.js
@@ -1,5 +1,6 @@
 import React, {Component} from 'react'
 import { useDisclosure, Text } from "@chakra-ui/core";
+import '../styles/Prescriptions.css';
 
 import {
     Modal,
@@ -17,16 +18,21 @@ import Navbar from './Navbar';
   function BasicUsage() {
     const { isOpen, onOpen, onClose } = useDisclosure();
     return (
-      <>
-        <Button onClick={onOpen}>Open Modal</Button>
+      <div className="prescription">
+        <Button width="200px" bg="#C6F6D5" variantColor="teal" variant="outline" height="123px" onClick={onOpen}>রিপোর্ট আইডি ie6y9<br/>এর প্রেসক্রিপশন</Button>
   
         <Modal isOpen={isOpen} onClose={onClose}>
           <ModalOverlay />
           <ModalContent>
-            <ModalHeader>Modal Title</ModalHeader>
+            <ModalHeader>রিপোর্ট আইডি ie6y9<br/>এর প্রেসক্রিপশন</ModalHeader>
             <ModalCloseButton />
             <ModalBody>
-                Yo
+            জ্বর এর জন্য ন্যাপা সেবন করুন। <br/>
+            করোনা সন্দেহে ইথানল সেবন করছিলেন। সেটি আশু বন্ধ করুন। <br/>
+            গলা ব্যাথার জন্য  acetaminophen সেবন করতে পারেন। <br/><br/>
+
+            ডঃ ইসমাইল - ০১৮১২৩৪৫৬৭৮ <br/>
+            ইন্টার্ন, ময়মনসিংহ মেডিকেল কলেজ 
               {/* <Lorem count={2} /> */}
             </ModalBody>
   
@@ -34,11 +40,11 @@ import Navbar from './Navbar';
               <Button variantColor="blue" mr={3} onClick={onClose}>
                 Close
               </Button>
-              <Button variant="ghost">Secondary Action</Button>
+              <Button bg="#CEEDFF">Further Communication</Button>
             </ModalFooter>
           </ModalContent>
         </Modal>
-      </>
+      </div>
     );
   }
 
@@ -47,7 +53,18 @@ class Prescriptions extends Component {
         return (
             <>
                 <Navbar/>
-                <BasicUsage/>
+                <div className="prescriptions" width="100%">
+                  <BasicUsage/>
+                  <BasicUsage/>
+                  <BasicUsage/>
+                  <BasicUsage/>
+                  <BasicUsage/>
+                  <BasicUsage/>
+                  <BasicUsage/>
+                  <BasicUsage/>
+                  <BasicUsage/>
+                  <BasicUsage/>
+                </div>
             </>
         )
     }
