22645bab64f68c565e88d2ac2f885d1cda3428f9
r00t <algocerebro@gmail.com>
2020-04-14 11:09:19 +0600

    yo

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -17,19 +17,19 @@ import { Link } from "@reach/router"
           return (
             <Breadcrumb className="navbar" bg="#FEFCBF" width="100%" height="100px" spacing="20px">
                 <BreadcrumbItem>
-                    <BreadcrumbLink className="link" color="#5F370E" href={process.env.PUBLIC_URL + '/'}>সেবা সম্পর্কে</BreadcrumbLink>
+                    <BreadcrumbLink className="link" color="#5F370E" as={Link} to={process.env.PUBLIC_URL + '/'}>সেবা সম্পর্কে</BreadcrumbLink>
                 </BreadcrumbItem>
 
                 <BreadcrumbItem>
-                    <BreadcrumbLink className="link" color="#5F370E" href={process.env.PUBLIC_URL + '/prescriptions'}>প্রেসক্রিপশন সমূহ</BreadcrumbLink>
+                    <BreadcrumbLink className="link" color="#5F370E" as={Link} to={process.env.PUBLIC_URL + '/prescriptions'}>প্রেসক্রিপশন সমূহ</BreadcrumbLink>
                 </BreadcrumbItem>
 
                 <BreadcrumbItem>
-                    <BreadcrumbLink className="link" color="#5F370E" href={process.env.PUBLIC_URL + '/report'}>রিপোর্ট তৈরি করুন</BreadcrumbLink>
+                    <BreadcrumbLink className="link" color="#5F370E" as={Link} to={process.env.PUBLIC_URL + '/report'}>রিপোর্ট তৈরি করুন</BreadcrumbLink>
                 </BreadcrumbItem>
 
                 <BreadcrumbItem>
-                    <BreadcrumbLink className="link" color="#5F370E" href="#">লগ আউট</BreadcrumbLink>
+                    <BreadcrumbLink className="link" color="#5F370E" as={Link} to="#">লগ আউট</BreadcrumbLink>
                 </BreadcrumbItem> 
             </Breadcrumb>              
           )
